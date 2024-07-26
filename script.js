94458ad02af468f66f7140ff158d40ff9dd6e7b8
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-07-26 12:12:41 +0100

    Add function to place mines

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const boardSize = 12;
+const numberOfMines = Math.floor(boardSize * boardSize * 0.1);
 const boardElement = document.querySelector(".board");
+const tiles = [];
 
 boardElement.style.setProperty('--size', boardSize);
 
@@ -11,3 +13,17 @@ for (let i = 0; i < boardSize; i++) {
         boardElement.appendChild(tile);
     }
 }
+
+function placeMines() {
+    let minesPlaced = 0;
+    while (minesPlaced < numberOfMines) {
+        const index = Math.floor(Math.random() * tiles.length);
+        const tile = tiles[index];
+        if (tile.dataset.mine !== "true") {
+            tile.dataset.mine = "true";
+            minesPlaced++;
+        }
+    }
+}
+
+placeMines();
