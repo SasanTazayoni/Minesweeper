aa1d63270d9f491ce021afd1af740267272e3945
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-07-26 12:30:36 +0100

    Adjust for loop that populates the board

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const tiles = [];
 boardElement.style.setProperty('--size', boardSize);
 
 for (let i = 0; i < boardSize; i++) {
-    for (let j = 0; j < boardSize; j++) {
-        const tile = document.createElement("div");
-        tile.classList.add("tile");
-        tile.dataset.status = "hidden";
-        boardElement.appendChild(tile);
+        for (let j = 0; j < boardSize; j++) {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.dataset.status = "hidden";
+            tile.dataset.row = i;
+            tile.dataset.col = j;
+            boardElement.appendChild(tile);
+            tiles.push(tile);
+        }
     }
-}
 
 function placeMines() {
     let minesPlaced = 0;
