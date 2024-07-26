c2e6405ca4e33eee57bbf29d1baa8a9030c88d92
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-07-26 16:14:19 +0100

    Add win and lose conditions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const boardElement = document.querySelector(".board");
 const minesLeft = document.querySelector(".subtext");
 const tiles = [];
 let flaggedCount = 0;
+let gameOver = false;
 
 function updateMinesLeft(count) {
     minesLeft.textContent = `Mines Left: ${count}`;
@@ -12,16 +13,16 @@ function updateMinesLeft(count) {
 boardElement.style.setProperty('--size', boardSize);
 
 for (let i = 0; i < boardSize; i++) {
-        for (let j = 0; j < boardSize; j++) {
-            const tile = document.createElement("div");
-            tile.classList.add("tile");
-            tile.dataset.status = "hidden";
-            tile.dataset.row = i;
-            tile.dataset.col = j;
-            boardElement.appendChild(tile);
-            tiles.push(tile);
-        }
+    for (let j = 0; j < boardSize; j++) {
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.dataset.status = "hidden";
+        tile.dataset.row = i;
+        tile.dataset.col = j;
+        boardElement.appendChild(tile);
+        tiles.push(tile);
     }
+}
 
 function placeMines() {
     let minesPlaced = 0;
@@ -37,6 +38,8 @@ function placeMines() {
 
 boardElement.addEventListener("contextmenu", e => {
     e.preventDefault();
+    if (gameOver) return;
+    
     const tile = e.target;
     if (tile.classList.contains("tile")) {
         if (tile.dataset.status === "hidden") {
@@ -52,20 +55,44 @@ boardElement.addEventListener("contextmenu", e => {
 });
 
 boardElement.addEventListener("click", e => {
+    if (gameOver) return;
+
     const tile = e.target;
     if (tile.classList.contains("tile")) {
         if (tile.dataset.status === "flagged") {
             return;
         }
         if (tile.dataset.mine === "true") {
-            console.log("Mine!");
             tile.dataset.status = "mine";
+            revealAllMines();
+            updateGameStatus('lose');
         } else {
-            console.log("Number");
             tile.dataset.status = "number";
+            const unrevealedTiles = tiles.filter(tile => tile.dataset.status === "hidden" && tile.dataset.mine !== "true");
+            if (unrevealedTiles.length === 0) {
+                updateGameStatus('win');
+            }
         }
     }
 });
+
+function revealAllMines() {
+    tiles.forEach(tile => {
+        if (tile.dataset.mine === "true") {
+            tile.dataset.status = "mine";
+        }
+    });
+}
+
+function updateGameStatus(condition) {
+    if (condition === 'lose') {
+        console.log("Lose");
+        gameOver = true;
+    } else if (condition === 'win') {
+        console.log("Win");
+        gameOver = true;
+    }
+}
 
 placeMines();
 updateMinesLeft(numberOfMines);
